10b1e2ba02895e8f49e94cb472759c70ced20d96
moriyama <kenshumoriyama01@gmail.com>
2019-04-15 16:19:34 +0900

    Euclid_roop

--- Euclid.rb
+++ Euclid.rb
@@ -3,11 +3,11 @@ big = gets.to_i
 puts "小さい方の初期値を入力"
 small = gets.to_i
 
-while (1)
+while (small != 0)
     ans = small;
     small = big%small;
     big = ans;
-    break if small == 0
+    #break if small == 0
 end
-p "最大公約数は"
+p "最大公約数"
 puts ans
