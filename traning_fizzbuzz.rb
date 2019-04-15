81e0102426a0b35c8c3601e9a13ca483fbd474bc
moriyama <kenshumoriyama01@gmail.com>
2019-04-15 16:32:40 +0900

    roop

--- traning_fizzbuzz.rb
+++ traning_fizzbuzz.rb
@@ -1,3 +1,4 @@
+=begin
 1.upto(100){|n|
     case
         when n%3 == 0 then
@@ -10,5 +11,20 @@
             puts n
             puts ("FIZZBUZZ")
         else
+    end
+}
+=end
+1.upto(100){|n|
+    if n%3 == 0
+        puts n
+        puts ("FIZZ")
+    end
+    if n%5 == 0
+        puts n
+        puts ("BUZZ")
+    end
+    if n%15 == 0
+        puts n
+        puts ("FIZZBUZZ")
     end
 }
