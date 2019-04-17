1003dd6fa51bdbb2d943d62594337d9f57e47d5b
moriyama <kenshumoriyama01@gmail.com>
2019-04-17 09:28:31 +0900

    カウント用ループから変数削除

--- traning3_hash.rb
+++ traning3_hash.rb
@@ -2,10 +2,9 @@ puts "文字列を入力"
 ans = gets.to_s.chomp
 hash1 = Hash.new
 
-a=1
 ans.each_char{|chr|
     if(hash1.include?(chr))
-        hash1[chr] += a
+        hash1[chr] += 1
     else
         hash1[chr] = 1
     end
