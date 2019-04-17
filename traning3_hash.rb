a7ad6a3d9a72fe80e0a5a30b9f120ac65b8c58ce
moriyama <kenshumoriyama01@gmail.com>
2019-04-17 09:03:37 +0900

    first

---
+++ traning3_hash.rb
@@ -0,0 +1,28 @@
+puts "文字列を入力"
+ans = gets.to_s.chomp
+hash1 = Hash.new
+
+ans.each_char{|chr|
+    a=1
+    if(hash1.include?(chr))
+        hash1[chr] += a
+    else
+        hash1[chr] = 1
+    end
+    puts hash1
+}
+
+max_count = 0
+max_chr = ""
+hash1.each{|chr,cnt|
+    if(cnt > max_count) then
+        max_chr = chr
+        max_count = cnt
+    end
+}
+
+hash1.each{|chr,cnt|
+    if(cnt == max_count) then
+        p max_chr = chr
+    end
+}
